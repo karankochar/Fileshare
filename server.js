4ae17c262982972de48bbe5589d43e2d136dd436
Karan Kochar <karankochar30@gmail.com>
2021-01-15 03:14:28 +0530

    Upload route created

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.use('/api/files', require('./routes/files'));
+
+app.listen( PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
